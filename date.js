294a9ff81a64aba5183bf0f5dc33d624b32a4124
Kumar Spandan <75578279+5p7Ro0t@users.noreply.github.com>
2021-08-13 12:40:22 +0530

    Update date.js

--- date.js
+++ date.js
@@ -2,7 +2,7 @@ function datefunc(){
     let x = document.getElementById("date_id").value;
     //console.log(x);
     let res = x.split("-");
-    console.log(res[1], res[0],res[2]);
+    //console.log(res[1], res[0],res[2]);
     if(res[0] == undefined || res[1] == undefined || res[2] == undefined){
         document.getElementById('show').innerHTML = "Empty Value :( Try Again!!"
     }
@@ -24,45 +24,54 @@ function age_calculator(year,month, date){
     let today_date = today.getDate();
     let today_year = today.getFullYear();
     //console.log(today_month + " " + today_date + " " + today_year);
-    let age_month , age_date = 0;
-    let birth_year = year;
-    let birth_month = month;
-    let birth_date = date;
-    let age_year = today_year - birth_year;
-    if(today_month < birth_month){
-        age_year--;
-        age_month = 12 - (birth_month - today_month);
-        if(today_date < birth_date ){
-            age_month--;
-            age_date = totaldays - (birth_date - today_date);
-        }
-        else{
-            age_date = today_date - birth_date;
-        }
+    if((date > today_date && (month >= today_month || year >= today_year)) || month > today_month || year > today_year){
+        document.getElementById('show').innerHTML = "Hello Friend , You can't be Earlier than today :)"
     }
-    else if(today_month == birth_month){
-        if(today_date < birth_date){
-            age_month = 11;
-            age_year--;
-            age_date = totaldays - (birth_date - today_date);
-        }
-        else{
-            age_month = 0;
-            age_date = today_date - birth_date;
-        }
+    else if(date == today_date && month == today_month && year == today_year){
+        document.getElementById('greet').innerHTML = "Congratulations to your Parents,"
+        document.getElementById('show').innerHTML = "You born today :)"
     }
     else{
-        age_month = today_month - birth_month;
-        if(today_date < birth_date ){
-            age_month--;
-            age_date = totaldays - (birth_date - today_date);
+        let age_month , age_date = 0;
+        let birth_year = year;
+        let birth_month = month;
+        let birth_date = date;
+        let age_year = today_year - birth_year;
+        if(today_month < birth_month){
+            age_year--;
+            age_month = 12 - (birth_month - today_month);
+            if(today_date < birth_date ){
+                age_month--;
+                age_date = totaldays - (birth_date - today_date);
+            }
+            else{
+                age_date = today_date - birth_date;
+            }
+        }
+        else if(today_month == birth_month){
+            if(today_date < birth_date){
+                age_month = 11;
+                age_year--;
+                age_date = totaldays - (birth_date - today_date);
+            }
+            else{
+                age_month = 0;
+                age_date = today_date - birth_date;
+            }
         }
         else{
-            age_date = today_date - birth_date;
+            age_month = today_month - birth_month;
+            if(today_date < birth_date ){
+                age_month--;
+                age_date = totaldays - (birth_date - today_date);
+            }
+            else{
+                age_date = today_date - birth_date;
+            }
         }
-    }
-    document.getElementById('greet').innerHTML = "Hello Friend, You are"
-    let age = age_year + " years " + age_month + " months " + age_date + " days Old. :)";
-    document.getElementById("show").innerHTML = age;
+        document.getElementById('greet').innerHTML = "Hello Friend, You are";
+        let age = age_year + " years " + age_month + " months " + age_date + " days Old. :)";
+        document.getElementById("show").innerHTML = age;
     //console.log(age_year + " " + age_month + " " + age_date);
+    }
 }
